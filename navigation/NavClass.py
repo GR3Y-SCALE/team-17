c78459361b67f67ee4a295b189f62e8a9752e64a
Maurice Blake <mauricertownsend@gmail.com>
2025-09-08 20:32:34 +1000

    tweak

--- navigation/NavClass.py
+++ navigation/NavClass.py
@@ -1,8 +1,11 @@
 from navigation import state_machine as sm
+from navigation import NavMath as navmath
 
 import numpy as np
 import math 
 import time
+
+debug = True
 
 class NavClass:
 
@@ -20,11 +23,17 @@ class NavClass:
         self.wicked_sm.update_state()
 
         # compute potential fields from camera view
-        self.computePotentialField() # Feed in useful data
+        self.compute_attractor_field() # Feed in useful data
 
-    def computePotentialField():
+    def compute_attractor_field(self, goal_deg): # feed in angle of goal
+        self.attraction_field[goal_deg]
+        gradient = 1 / 30
+        for angle in range(0, int((CAMERA_FOV/2 + 1))):
+            self.attractive_field[navmath.clip]
+
         # get OM from camera
         # get GD from camera
         # Motor heading map generated from (GD + OM)
 
         # get maximum peak to find path to travel, noting the peak is the angle of the motors and the magnitude is speed
+
--- navigation/state_machine.py
+++ navigation/state_machine.py
@@ -1,27 +1,79 @@
 import time
 
+
 class State:
     def __init__(self):
         # Runs when state is instantiated
-        print('current state: ',str(self))
+        print('current state: ', str(self))
 
-    def run(self):
+    def run(self, *args, **kwargs):
         pass
 
     def __repr__(self):
         return self.__str__()
-    
+
     def __str__(self):
         return self.__class__.__name__
+
+
+class StartState(State):
+    """
+    Run start-up procedures for nav. If startup does not complete,
+    raise an exception (implements the TODO).
+    """
+
+    def __init__(self, event=None):
+        super().__init__()
+        self.event = event
+
+    def run(self, event=None):
+        if event is not None:
+            self.event = event
+        # startup robot, home gripper, initialise motors cameras...
+        if self.event != 'startup_done':
+            raise RuntimeError('Robot failed to start')
+
+        # Otherwise, continue normally (no-op here)
+        return 'startup_done'
     
-    class startState(State):
-        # run start up procedures for nav stuff
+class findPickingStation(State):
+    def run(self):
+        print('Searching for picking station...')
 
-        event = 'startup_done'
+        # turn robot, look, turn and so on until found. Some timeout condition if robot turns around full 360.
 
-        try event == 'startup_done':
+        if event == 'found_picking_station':
+            return collectFromPickingStation()
+        else:
+            # report unsuccessgful search
 
-            except:
-            #TODO: rasise exception for robot failing to start
-                raise Exception
-         
+class moveToShelf(State):
+    def run(self):
+        print('moving to shelf...')
+
+        # move to shelf
+
+        if event == 'arrived_at_shelf':
+            return placeItemOnShelf()
+        elif event == '...':
+            # extra logic to handle not arriving at the shelf
+
+class leaveShelf(State):
+    def run(self):
+        print('leaving shelf')
+        # leave shelf
+
+        if event == 'left_shelf':
+            return findPickingStation()
+        elif event == '...':
+            # keep trying to leave
+
+
+
+class stateMachine():
+    def __init__(self):
+        self.state = StartState()
+
+    def update_state(self):
+        self.state = self.state.run()
+
