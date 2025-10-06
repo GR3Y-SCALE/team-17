0b5cc16307ff13666d04b668e22ff671d5c48bf4
Maurice Blake <mauricertownsend@gmail.com>
2025-10-06 18:01:19 +1000

    testing drive

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
 import os,sys,time
+from mobility.drive_system import DriveSystem
+from navigation.NavClass import NavClass
 
-
+nav = NavClass(FOV=180, width=0.16)
+robot = DriveSystem()
 
 def main():
+    print(robot.get_wheel_speeds())
+    time.sleep(1)
 
 
 
