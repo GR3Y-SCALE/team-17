4cd5652f6f6c4f86a2f9e3874f8308c2a75d1656
Maurice Blake <mauricertownsend@gmail.com>
2025-10-24 15:48:57 +1000

    Keyboard control

--- mobility/keyboardControl.py
+++ mobility/keyboardControl.py
@@ -9,39 +9,9 @@ sys.path.append("../")
 
 import time
 
-from DFRobot_RaspberryPi_DC_Motor import THIS_BOARD_TYPE, DFRobot_DC_Motor_IIC as Board
+from mobility.drive_system import DriveSystem
 
-
-board = Board(1, 0x10)
-
-# Global duty cycle variable
-duty = 75
-lDuty = 0.9*duty
-duty_step = 2  # how much to increase/decrease with +/-
-
-def board_detect():
-    l = board.detecte()
-    print("Board list conform:")
-    print(l)
-
-def print_board_status():
-    if board.last_operate_status == board.STA_OK:
-        print("board status: everything ok")
-    elif board.last_operate_status == board.STA_ERR:
-        print("board status: unexpected error")
-    elif board.last_operate_status == board.STA_ERR_DEVICE_NOT_DETECTED:
-        print("board status: device not detected")
-    elif board.last_operate_status == board.STA_ERR_PARAMETER:
-        print("board status: parameter error, last operate no effective")
-    elif board.last_operate_status == board.STA_ERR_SOFT_VERSION:
-        print("board status: unsupported board firmware version")
-
-
-
-
-
-
-
+robot = DriveSystem()
 
 
 
@@ -49,10 +19,6 @@ def handRPM():
     import math
     from time import sleep, monotonic
     poll_dt = 0.01
-    board.set_encoder_enable([board.M1, board.M2])
-    board.set_encoder_reduction_ratio([board.M1, board.M2], int(50))
-    circ = math.pi*(30/1000)
-
     t0 = monotonic()
     sL = sR = 0.0
     t_prev = monotonic()
@@ -64,84 +30,35 @@ def handRPM():
         dt = t_now - t_prev
         t_prev = t_now
 
-        # encoders -> distance
-        rpmL, rpmR = board.get_encoder_speed([board.M1, board.M2])
-        revL = abs(rpmL) * dt / 60.0
-        revR = abs(rpmR) * dt / 60.0
-
-        sL += revL * circ
-        sR += revR * circ
-        s_enc  = 0.5 * (sL + sR)
-
-        print("DEBUGGING:")
-        print("revL", revL)
-        print("revR", revR)
-        print("sL", sL)
-        print("sR", sR)
-        print("s_enc", s_enc)
 
 def on_key_release(key):
-    board.motor_stop(board.ALL)
+    robot.stop_all()
 
 def on_key_pressed(key):
     global duty
     global lDuty
 
     if key == Key.right:
-        print(f"Right key pressed at duty {duty}%")
-        board.motor_movement([board.M1], board.CW, duty)
-        board.motor_movement([board.M2], board.CW, duty)
+        robot.set_target_velocities(0.0,50)
 
     elif key == Key.left:
-        print(f"Left key pressed at duty {duty}%")
-        board.motor_movement([board.M1], board.CCW, duty)
-        board.motor_movement([board.M2], board.CCW, duty)
+        robot.set_target_velocities(0.0,-50)
+
 
     elif key == Key.up:
-        print(f"Up key pressed at duty {duty}%")
-        board.motor_movement([board.M1], board.CCW, duty)
-        board.motor_movement([board.M2], board.CW, lDuty)
+        robot.set_target_velocities(0.5,0.0)
+
 
     elif key == Key.down:
-        print(f"Down key pressed at duty {duty}%")
-        board.motor_movement([board.M1], board.CW, duty)
-        board.motor_movement([board.M2], board.CCW, lDuty)
+        robot.set_target_velocities(-0.5,0.0)
 
     elif key == Key.esc:
-        print("ESC pressed: stopping motors for 3s")
-        board.motor_stop(board.ALL)
-        time.sleep(3)
+        robot.stop_all()
     
     
-
-    # Adjust duty with + and -
-    elif hasattr(key, 'char') and key.char == '+':
-        duty = min(100, duty + duty_step)
-        lDuty = 0.9*duty
-        print(f"Duty increased to {duty}%")
-
-    elif hasattr(key, 'char') and key.char == '-':
-        duty = max(0, duty - duty_step)
-        lDuty = 0.9*duty
-        print(f"Duty decreased to {duty}%")
 
 if __name__ == "__main__":
     print("Starting...")
-
-    #initServos()
-    #centre()
-    board_detect()
-
-    while board.begin() != board.STA_OK:
-        print_board_status()
-        print("board begin failed")
-        time.sleep(2)
-
-    print("board begin success")
-    board.set_encoder_enable(board.ALL)
-    board.set_encoder_reduction_ratio(board.ALL, 50)
-    board.set_moter_pwm_frequency(2000)
-    
 
     with keyboard.Listener(on_press=on_key_pressed, on_release=on_key_release) as listener:
         listener.join()
